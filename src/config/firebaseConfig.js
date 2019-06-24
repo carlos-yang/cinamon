fc1281bdbef53659a766644adbc7b501f59f0a70
james <mr.jamse.yang@gmail.com>
2019-06-24 09:44:40 +0900

    fiarebase 추가

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyDuCDcoIhtQlOWKF3qrNhqty6LN84yOTJw',
+  authDomain: 'cinamon-fb.firebaseapp.com',
+  databaseURL: 'https://cinamon-fb.firebaseio.com',
+  projectId: 'cinamon-fb',
+  storageBucket: 'cinamon-fb.appspot.com',
+  messagingSenderId: '842873036367'
+};
